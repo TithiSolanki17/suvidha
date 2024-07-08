3c25835b8c19a0ec645552c070f1a62da768e1c5
Kaonashi2486 <sakshigolatkar123@gmail.com>
2024-07-08 22:18:34 +0530

    testimonials head

---
+++ src/components/testimonial_head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function Slogan() {
+  return (
+    <div className="h-auto md:h-[20rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 text-black">
+    <div className="p-4 relative z-10 w-full text-center text-4xl ">
+      <h1><b>Our Testimonials</b></h1>
+      </div>
+    <div className="p-4 relative z-10 w-full text-center">
+      </div>
+      </div>
+  )
+}
+export default Slogan;
