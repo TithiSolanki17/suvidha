29c5b5054d7af9ddfb7dd801d58d8476dc3a2229
TithiSolanki17 <tithisolanki@gmail.com>
2024-07-08 23:07:50 +0530

    3

--- src/components/abovenav.tsx
+++ src/components/abovenav.tsx
@@ -28,7 +28,7 @@ const people = [
   },
   {
     id: 5,
-    name: "phone",
+    name: "Phone",
     image:
       "https://images.unsplash.com/photo-1611606063065-ee7946f0787a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
