ba2a9a05318983f50651db7f035f4baf23d727fb
Kaonashi2486 <sakshigolatkar123@gmail.com>
2024-07-13 16:14:01 +0530

    adding the suvidha logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import AnimatedTooltipPreview from "@/components/abovenav";
+import Logo from "@/components/ui/logo";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Logo />
         <Navbar/>
         <div>
       <AnimatedTooltipPreview/>
