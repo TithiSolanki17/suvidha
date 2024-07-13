870a6a95a5a60f42a2d8ba7fd721288ce4eb63f8
Kaonashi2486 <sakshigolatkar123@gmail.com>
2024-07-14 00:20:42 +0530

    home page start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import NumberTickerDemo from "@/components/ui/home_ui/numberticker";
 // 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white" style={{ color: 'black' }}>
-     homepage
+     <NumberTickerDemo value={90}/>
     </main>
   );
 }
