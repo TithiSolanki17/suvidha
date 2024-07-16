a1365af968aef92b0e4176e07d4b3cf72280f813
Sakshi Golatkar <128234909+Kaonashi2486@users.noreply.github.com>
2024-07-16 19:52:27 +0530

    Update Eventslider.tsx

--- src/components/Eventslider.tsx
+++ src/components/Eventslider.tsx
@@ -1,33 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default function Eventslider(){
+export default function EventSlider() {
   return (
     <>
-    <div className="slide-container">
+      <div className="slide-container">
         <div className="slide-content">
-            <div className="card-wrah6h6er">
-                <div className="card">
-                    <div className="image-content">
-                        <span className="overlay"></span>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="image-content">
+                <span className="overlay"></span>
 
-                        <div className="card-image">
-                            <image>image</image>
-                        </div>
-                    </div>
-                    <div className="card-content"></div>
-                    <div>
-                        <h2 className="name">Tree plantation</h2>
-                       <h6 className="description">
+                <div className="card-image">
+                  <img src="image-url" alt="Image Alt Text" />
+                </div>
+              </div>
+              <div className="card-content">
+                <div>
+                  <h2 className="name">Tree plantation</h2>
+                  <h6 className="description">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Deleniti dignissimos atque, corrupti unde ab quae aliquam
                     rerum corporis ipsum exercitationem eveniet non maiores
                     dicta? Laudantium sint hic asperiores eligendi autem?
                   </h6>
-                    </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
