3ab382852f9d205e486fd4b4846a37c60b570d59
Sakshi Golatkar <128234909+Kaonashi2486@users.noreply.github.com>
2024-07-16 20:29:43 +0530

    Update sticky-scroll-reveal.tsx

--- src/components/home_components/sticky-scroll-reveal.tsx
+++ src/components/home_components/sticky-scroll-reveal.tsx
@@ -57,7 +57,7 @@ export const StickyScroll = ({
 
   useEffect(() => {
     setBackgroundGradient(linearGradients[activeCard % linearGradients.length]);
-  }, [activeCard]);
+  }, [activeCard, linearGradients]); // Include linearGradients in the dependency array
 
   return (
     <motion.div
